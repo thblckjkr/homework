8c0d4ba996015c35abe58b1b4bb0e2a67cb5ea83
Teo Gonzalez <teo.gonzalez.c@hotmail.com>
2019-10-10 19:55:15 -0600

    Delete non necessary things

--- java_rmi/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-//package UACJRMI;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloWorld extends Remote {
-	public String sayHello(String who) throws RemoteException;
-}
