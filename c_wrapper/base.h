66271413433c2fbc27226800cbdfe68d7ef4ceec
Teo Gonzalez <contact@thblckjkr.tk>
2019-11-03 17:45:45 -0700

    Add base to project

---
+++ c_wrapper/base.h
@@ -0,0 +1,14 @@
+// Impresora
+void ImprimeImpresora(char * nombre_archivo);
+int numeroTrabajosEsperaImpresora();
+void LimpiaPrintingQueueImpresora();
+
+// Plotter
+void ImprimePlotter(char * nombre_archivo);
+int numeroTrabajosEsperaPlotter();
+void LimpiaPrintingQueuePlotter();
+
+// Caja registradora 
+void AbreCajaRegistradora();
+float totalAPagar();
+void EstableceMontoAPagar(float cantidad);
