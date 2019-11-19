ed2eaec7f6109139b53572839a382074038e6ae1
Teo Gonzalez <contact@thblckjkr.tk>
2019-11-18 22:16:49 -0700

    Client seems to work

--- scripts_sockets_python/Addressbook_client.py
+++ scripts_sockets_python/Addressbook_client.py
@@ -1,14 +1,39 @@
 # Import of libraries
 import socket               # Import main socket library
 from Utils import Protocol  # General program utilities
+from Utils import UI
+
+class Socket:
+
+   def init(self, ip, port):
+      self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.s.connect((ip, port)) 
+
+   def exchangeMessage(self, message):
+      self.s.send(message)
+      x = self.s.recvmsg()
+
+      print (x)
+      return x
 
 p = Protocol()
+s = Socket()
+ui = UI()
+
 
 X = p.CreateMessage("Mi mama me mima", True)
 
-"""
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 8089)) 
-  
-clientsocket.send('hello')
-"""
+ui.show("Bienvenido al sistema de consulta de correos")
+server = ui.ask("Para empezar, escribe la direccion IP del servidor")
+
+s.init(server, 5678)
+
+while(True):
+   request = ui.ask("Cual es el nombre de la persona que buscas?")
+   data = p.CreateMessage(request, True)
+   response = s.exchangeMessage(data)
+   ui.show(response)
+
+   cont = ui.askYesNo("Deseas continuar con la ejecución")
+   if (not cont):
+      exit
--- scripts_sockets_python/Utils.py
+++ scripts_sockets_python/Utils.py
@@ -44,3 +44,49 @@ class Protocol:
    def getData(self, data):
       data = data.strip()
       return data.substring[1]
+
+# Teo Gonzalez Calzada [@thblckjkr] 2019/04/07
+# Class to act as an interface to the user
+
+class UI:
+   colors = {
+      'header' : '\033[95m', 'info' : '\033[94m', 'success' : '\033[92m', 'warning' : '\033[93m',
+      'error' : '\033[91m', 'ENDC' : '\033[0m', 'bold' : '\033[1m', 'underline' : '\033[4m'
+   }
+   def __init__(self):
+      self.show("Inicializando interfaz...\n", 'info')
+      
+   def show(self, message, type = 'info'):
+      color = self.colors.get(type, "Invalid month")
+      print (color + message + self.colors['ENDC'])
+
+   def askYesNo(self, message):
+      self.show(message, 'info')
+      message = "Presiona: \n [0] para NO\n [1] para SI\n Tu respuesta: "
+      while(True):
+         temp = input(message)
+         if temp != '0' and temp != '1':
+            self.show("No has seleccionado un valor coherente", 'warning')
+         else:
+            return int(temp)
+   
+   def askNumber(self, message, t ='int'):
+      self.show(message, 'info')
+      message = ""
+      while(True):
+         temp = input("")
+         try:
+            if t == 'float':
+               xtemp = float(temp)
+            else:
+               xtemp = int(temp)
+            
+            return xtemp
+         except ValueError:
+            self.show("No has seleccionado un valor coherente", 'warning')
+
+   def ask(self, message):
+      self.show(message, 'info')
+      message = ""
+      temp = input("")
+      return temp
